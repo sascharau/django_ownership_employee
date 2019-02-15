3ca21d19c3e2f56b96fd664a9a21f6036e6a446e
sascha <sascha@ideal3000.de>
2019-02-15 13:00:51 +0100

    add gitignore

--- ownership_employee/conf/settings.py
+++ ownership_employee/conf/settings.py
@@ -161,10 +161,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
